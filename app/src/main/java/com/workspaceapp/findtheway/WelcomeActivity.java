c8e1be039554948422cd6254c53477757727b4f9
WojciechKwasnyy <wojtekkwa@o2.pl>
2016-12-31 14:57:53 +0100

    Initial commit

---
+++ app/src/main/java/com/workspaceapp/findtheway/WelcomeActivity.java
@@ -0,0 +1,34 @@
+package com.workspaceapp.findtheway;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Typeface tf =  Typeface.createFromAsset(getAssets(), "FTW.ttf");
+        Typeface tf2 =  Typeface.createFromAsset(getAssets(), "Sweet Sensations Personal Use.ttf");
+
+        ImageView gif = (ImageView) findViewById(R.id.animationView);
+        Animation logogif = AnimationUtils.loadAnimation(this, R.anim.animationgif);
+        gif.startAnimation(logogif);
+        TextView textViewLoading = (TextView) findViewById(R.id.loading_textview);
+        textViewLoading.setTypeface(tf2);
+        TextView textViewTitle = (TextView) findViewById(R.id.title_textview);
+        textViewTitle.setTypeface(tf2);
+
+    }
+
+}
